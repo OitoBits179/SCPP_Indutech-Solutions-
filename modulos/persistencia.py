a38cb34b37f7ee6635e6f8ac451d42338233a515
OitoBits179 <oitobits486@gmail.com>
2026-09-10 08:56:54 -0300

    Add functions to load and save data in JSON format
    Implement data loading and saving functions with JSON.

--- modulos/persistencia.py
+++ modulos/persistencia.py
@@ -1,1 +1,73 @@
+import json
+import os
 
+
+ARQUIVO = "banco_dados.json"
+
+
+def carregar_dados():
+
+    if not os.path.exists(ARQUIVO):
+
+        try:
+
+            with open(
+                ARQUIVO,
+                "w",
+                encoding="utf-8"
+            ) as f:
+
+                json.dump(
+                    [],
+                    f,
+                    indent=4,
+                    ensure_ascii=False
+                )
+
+        except Exception:
+            pass
+
+        return []
+
+    try:
+
+        with open(
+            ARQUIVO,
+            "r",
+            encoding="utf-8"
+        ) as f:
+
+            dados = json.load(f)
+
+            if isinstance(dados, list):
+                return dados
+
+            return []
+
+    except Exception:
+
+        return []
+
+
+def salvar_dados(dados):
+
+    try:
+
+        with open(
+            ARQUIVO,
+            "w",
+            encoding="utf-8"
+        ) as f:
+
+            json.dump(
+                dados,
+                f,
+                indent=4,
+                ensure_ascii=False
+            )
+
+        return True
+
+    except Exception:
+
+        return False
